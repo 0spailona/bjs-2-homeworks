d666681c7db02109b4fb9b7164f4e98a1d24b4bb
Daria <n-d-p@mail.ru>
2023-07-28 23:59:38 +0300

    Ready.

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.hasOwnProperty('marks')) {
+        this.marks.push(...marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    return this.marks ? this.marks.reduce((sum, mark) => sum + mark / this.marks.length, 0) : 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+if (this.marks.length === 0){
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;
 }
+}
+new Student("Василиса", "женский", 19);
+new Student("Алексей", "мужской", 21);
